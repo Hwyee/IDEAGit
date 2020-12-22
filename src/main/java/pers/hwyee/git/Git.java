fc55f350d8d2146980464d991fbbba58d578b3b4
tom_glb <goodMorning_glb@hwyee.com>
2020-12-22 19:14:01 +0800

    idea commit

---
+++ src/main/java/pers/hwyee/git/Git.java
@@ -0,0 +1,15 @@
+package pers.hwyee.git;
+
+/**
+ * @author hwyee
+ * @version 1.0
+ * @Description
+ * @date 2020-12-22 17:16
+ */
+
+
+public class Git {
+    public static void main(String[] args) {
+        System.out.println("Git");
+    }
+}
---
+++ src/test/java/TestGit.java
@@ -0,0 +1,18 @@
+
+import org.junit.Test;
+
+/**
+ * @author hwyee
+ * @version 1.0
+ * @Description
+ * @date 2020-12-22 17:17
+ */
+
+
+public class TestGit {
+    @Test
+    public void test(){
+        System.out.println("TestGit");
+    }
+
+}
